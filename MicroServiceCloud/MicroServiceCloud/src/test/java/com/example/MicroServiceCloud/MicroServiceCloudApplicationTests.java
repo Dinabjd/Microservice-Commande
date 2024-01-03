8a9033ea31f356c6416753e74f426837f3e1e257
Dinabjd <dinabjd2@gmail.com>
2024-01-03 19:17:20 +0100

    Cloud Configuration

---
+++ MicroServiceCloud/MicroServiceCloud/src/test/java/com/example/MicroServiceCloud/MicroServiceCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MicroServiceCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
